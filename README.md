d1f408e6dd2b2f882e4b96999bb24781fe8777df
Khushbu <singhkhushbu210@gmail.com>
2021-03-27 17:02:09 +0530

    jwt implemented

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # trial-reg-form
+
+Available Scripts
+In the project directory, you can run:
+
+node app.js
+Runs this repository in the development mode.
+Open http://localhost:9999 to view it in the browser.
+
+The page will not reload if you make edits, you have to re-run it.
+You will also see any lint errors in the console.
--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const mongoose = require('mongoose')
 const User = require('./model/user')
 const bcrypt = require('bcryptjs')
 const { ok } = require('assert')
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET = 'hnuheduhdy83yeudhuh1293-982ucfhfc(:")@#$%^34566*+=jbfcjkhueduf%$#U&%@@!@!*8gygyghgy'
+
+
 
 mongoose.connect('mongodb://localhost:27017/login-app-db', {
     useNewUrlParser: true,
@@ -23,10 +28,27 @@ app.use(bodyParser.json())
 app.post('/api/login', async (req, res) => {
     // console.log(req.body)
     const { username, password } = req.body
+    const user = await User.findOne({ username }).lean()
 
-    const user = await User.findOne({ username, password }).lean()
+    if(!user) {
+        return res.json({status: 'error', error: 'Invalid username/password'})
+    }
 
-    res.json({status: 'ok', data: 'Coming Soon'})
+    if(await bcrypt.compare(password, user.password)) {
+        // the username,password combination is successful 
+
+        const token = jwt.sign({
+            id: user._id, 
+            username: user.username
+        }, 
+        JWT_SECRET
+        )
+
+        return res.json({status: 'ok', data: token })
+
+    }
+
+    res.json({status: 'error', error: 'Invalid username/password'})
 })
 
 app.post('/api/register', async (req, res) => {
